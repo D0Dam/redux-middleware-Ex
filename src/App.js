1edf23e7c721b04bef14f654bc8d8765afac6dca
KimMinJae <smallkdb@gmail.com>
2022-05-19 03:48:48 +0900

    learn redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import CounterContainer from "./Containers/CounterContainer";
-
+//import CounterContainer from "./Containers/CounterContainer";
+import SampleContainer from "./Containers/SampleContainer";
 const App = () => {
 	return (
 		<div>
-			<CounterContainer />
+			<SampleContainer />
 		</div>
 	);
 };
